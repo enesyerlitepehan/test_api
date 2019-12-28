e4585993a796faf927603f23c3d93448ebe7f81f
Enes Tepehan <etepehan@netrd.com.tr>
2019-12-28 19:52:52 +0300

    movie all and find movie_id

--- routes/movie.js
+++ routes/movie.js
@@ -4,6 +4,26 @@ const router = express.Router();
 //Models
 const Movie = require('../models/Movie')
 
+router.get('/', (req, res) => {
+  const promise = Movie.find({ });
+  promise.then((data) => {
+    res.json({data});
+  }).catch((err) => {
+    res.json(err);
+  })
+});
+
+router.get('/:movie_id', (req, res) => {
+  res.send(req.params);
+  const promise = Movie.findById(req.params.movie_id);
+
+  promise.then((movie) => {
+    res.json({movie});
+  }).catch((err) => {
+    res.json(err);
+  })
+});
+
 router.post('/', (req, res, next) => {
   const { title, imdb_score, category, country, year} = req.body;
 
