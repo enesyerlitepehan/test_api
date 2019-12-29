6102481486e5c8810764df8c1e2e4db2e1c8c759
Enes Tepehan <etepehan@netrd.com.tr>
2019-12-29 18:04:05 +0300

    movies/betwen

--- routes/movie.js
+++ routes/movie.js
@@ -13,11 +13,45 @@ router.get('/', (req, res) => {
   })
 });
 
+// Top 10 list
+router.get('/top10', (req, res) => {
+  const promise = Movie.find({ }).limit(10).sort({ imdb_score: -1});
+  promise.then((data) => {
+    res.json({data});
+  }).catch((err) => {
+    res.json(err);
+  })
+});
+
+
 router.put('/:movie_id', (req, res) => {
   const promise = Movie.findByIdAndUpdate(
       req.params.movie_id,
-      req.body
+      req.body,
+      {
+        new: true
+      }
       );
+
+
+  promise.then((movie) => {
+    if(!movie)
+      next({message: 'The movie was not found', code: 99});
+
+    res.json(movie);
+  }).catch((err) => {
+    res.json(err);
+  })
+});
+
+router.delete('/:movie_id', (req, res) => {
+  const promise = Movie.findByIdAndRemove(
+      req.params.movie_id,
+      req.body,
+      {
+        new: true
+      }
+  );
 
 
   promise.then((movie) => {
@@ -58,5 +92,19 @@ router.post('/', (req, res, next) => {
     res.json(err);
   });
 });
+
+// Between
+router.get('/between/:start_year/:end_year', (req, res) => {
+  const { start_year, end_year} = req.params;
+  const promise = Movie.find(
+      {
+        year: {"$gte": parseInt(start_year), "$lte": parseInt(end_year)}
+      }
+  );
+
+
+
+});
+
 
 module.exports = router;
