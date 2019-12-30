2d050a3e89d79aa7f15ea79ed612ea3534093822
Enes Tepehan <etepehan@netrd.com.tr>
2019-12-31 00:20:53 +0300

    jwt version 01

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 
 //Models
@@ -29,8 +30,46 @@ router.post('/register', (req, res, next) => {
             res.json(err);
         })
     });
-
-
 });
+
+router.post('/authenticate', (req, res, next) => {
+    const {username, password} = req.body;
+
+    User.findOne({
+        username
+    }, (err, user) => {
+        if (err)
+            throw err;
+
+        if (!user) {
+            res.json({
+                status: false,
+                message: 'Authenticate failed, user not found.'
+            });
+        } else {
+            bcrypt.compare(password, user.password).then((result) => {
+                if(!result){
+                    res.json({
+                        status: false,
+                        message: 'Authenticate failed, user not found.'
+                    });
+                }else{
+                    const payload = {
+                        username
+                    };
+                    const token = jwt.sign(payload, req.app.get('api_secret_key'), {
+                       expiresIn: 720 // 12 hours
+                    });
+
+                    res.json({
+                        status: true,
+                        token
+                    });
+                }
+            });
+        }
+    });
+});
+
 
 module.exports = router;
