c189e86f4ec7e0fca988df177d3719ab41ac9c6b
Enes Tepehan <etepehan@netrd.com.tr>
2020-01-02 21:34:21 +0300

    find movie test script added

--- routes/movie.js
+++ routes/movie.js
@@ -25,6 +25,20 @@ router.get('/', (req, res) => {
   })
 });
 
+router.get('/:movie_id', (req, res, next) => {
+  const promise = Movie.findById(req.params.movie_id);
+
+  promise.then((movie) => {
+    console.log(movie);
+    if (!movie)
+      next({ message: 'The movie was not found.', code: 99 });
+
+    res.json(movie);
+  }).catch((err) => {
+    res.json(err);
+  });
+})
+
 // Top 10 list
 router.get('/top10', (req, res) => {
   const promise = Movie.find({ }).limit(10).sort({ imdb_score: -1});
@@ -100,7 +114,7 @@ router.post('/', (req, res, next) => {
 
   const promise = movie.save();
   promise.then((data) => {
-    res.json({ status: 1});
+    res.json(data);
   }).catch((err) => {
     res.json(err);
   });
--- test/api/movie.test.js
+++ test/api/movie.test.js
@@ -2,7 +2,7 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const should = chai.should();
 const server = require('../../app');
-let token;
+let token, movieId;
 chai.use(chaiHttp);
 
 describe('Movies tests', () => {
@@ -23,12 +23,62 @@ describe('Movies tests', () => {
                 .get('/api/movies')
                 .set('x-access-token', token)
                 .end((err,res) => {
-                    console.log(err);
-                    console.log(res.body);
+                    //console.log(err);
+                    //console.log(res.body);
                     res.should.have.status(200);
                     res.body.data.should.be.a('array');
                     done();
                 });
         })
+    });
+
+    describe('/POST movie', () => {
+        it('it should POST a movie', (done) => {
+            const movie = {
+                title: 'Udemy',
+                director_id: '5e08e8e022914f4684f284b5',
+                category: 'Komedi',
+                country: 'Türkiye',
+                year: 1950,
+                imdb_score: 8
+            }
+            chai.request(server)
+                .post('/api/movies/')
+                .send(movie)
+                .set('x-access-token', token)
+                .end((err, res) => {
+                    //console.log(res.body);
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('title');
+                    res.body.should.have.property('director_id');
+                    res.body.should.have.property('category');
+                    res.body.should.have.property('country');
+                    res.body.should.have.property('year');
+                    res.body.should.have.property('imdb_score');
+                    movieId = res.body._id;
+                    done();
+                })
+        });
+    });
+
+    describe('/GET/:director_id movie', () => {
+        it('it should GET a movie by the given id', (done) => {
+            chai.request(server)
+                .get('/api/movies/' + movieId)
+                .set('x-access-token', token)
+                .end((err, res) => {
+                    //console.log(res.body);
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('title');
+                    res.body.should.have.property('director_id');
+                    res.body.should.have.property('category');
+                    res.body.should.have.property('country');
+                    res.body.should.have.property('year');
+                    res.body.should.have.property('_id').eql(movieId);
+                    done();
+                });
+        });
     });
 });
